5a696b82277694f8fcb9a6407a67e8f9dde11e1a
AlBovo <88632271+AlBovo@users.noreply.github.com>
2023-02-24 19:11:05 +0100

    Update Software 18 - Pwntools 2.py

--- Introduzione/Software Security ⚙️/Software 18 - Pwntools 2.py
+++ Introduzione/Software Security ⚙️/Software 18 - Pwntools 2.py
@@ -1,31 +1,30 @@
 from pwn import *
-HOST = 'software-18.challs.olicyber.it'
+HOST = 'software-18.challs.olicyber.it' # runnare con debug negli args
 PORT = 13001
 
 conn = remote(HOST, PORT)
 
-conn.recv(100)
-conn.send(b"s\n")
+conn.recv(10000)
+conn.sendline()
 
 for _ in range(100):
     try:
         conn.recvuntil(b"0x")
     except:
-        print(f)
         print(num)
         conn.interactive()
     num = conn.recvuntil(b" ").decode().strip()
     f = conn.recv(100)
     if b"unpacked" in f and b"64" in f:
-        conn.sendline(u64(num))
-        print("u64")
+        conn.send(u64(num))
+        #print("u64")
     elif b"unpacked" in f and b"32" in f:
-        conn.sendline(u32(num))
-        print("u32")
+        conn.send(u32(num))
+        #print("u32")
     elif b"packed" in f and b"64" in f:
-        conn.sendline(p64(int(num, 16), endianness='little'))
-        print("p64")
+        conn.send(p64(int(num, 16)))
+        #print("p64")
     elif b"packed" in f and b"32" in f:
-        conn.sendline(p32(int(num, 16), endianness="little"))
-        print("p32")
+        conn.send(p32(int(num, 16)))
+        #print("p32")
 print(conn.recv(100).decode())
