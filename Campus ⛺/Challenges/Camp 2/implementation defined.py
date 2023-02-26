72f295f34f29a34211405a28a7d7f85f34173787
Error404 <alandavide.bovo.stud@ispascalcomandini.it>
2023-02-26 21:26:16 +0100

    Nuove chall

---
+++ Campus ⛺/Challenges/Camp 2/implementation defined.py
@@ -0,0 +1,4 @@
+from pwn import *
+key = bytes.fromhex("f30f1efa4883ec08488b05d92f00004885c07402ffd04883c4")
+flag = b"\x95\x63\x7f\x9d\x33\xb2\xd9\x57\x3c\xe3\x34\xec\x70\x63\x30\x2c\xb6\x9f\x44\x70\xa0\xbe\x78\xf7\xb9"
+print(xor(key, flag).decode())
