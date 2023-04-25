06aaa0a476d9bde6c23ef1d0ee3a9f8764a9c1d6
AlBovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-04-25 17:13:47 +0200

    altre chall

--- Olicyber 2023 🏆/Territoriale/IndovinANDo segreti.py
+++ Olicyber 2023 🏆/Territoriale/IndovinANDo segreti.py
@@ -1,17 +1,24 @@
 from pwn import *
 from Crypto.Util.number import *
-r = remote("segreto.challs.olicyber.it", 33000, timeout=100000)
-r.recv(1000)
+r = remote("segreto.challs.olicyber.it", 33000)
+r.recvuntil(b"flag!\n")
 for i in range(10):
     maxi = -1
-    r.recv(1000)
+    f = r.recvuntil(b"10\n")
+    print(f)
+    assert(ord(str(i+1)) == f[6])
     for e in range(256):
-        r.sendline(str(e).encode())
-        f = r.recvline(True)
-        print(f)
+        r.recv(100)
+        try:
+            r.sendline(str(e).encode())
+        except:
+            print(e-1)
+        f = r.recvuntil(b"\n")
+        #print(f)
+        print(e)
         maxi = max(maxi, int(f.decode().replace(">", "").strip(), 16))
     r.recv(100)
     r.sendline(b"g")
-    r.recv(1000)
+    print(r.recv(1000))
     r.sendline(long_to_bytes(maxi).hex().encode())
 print(r.recv(100).decode())
